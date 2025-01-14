36c29f3ad3a141ce80eec6196f7f11f3609462f4
davedumto <ejeredavid2001@gmail.com>
2025-01-14 11:29:17 +0100

    feat:Worked on the responsiveness of the contact us page

--- src/app/contact/components/contactform.tsx
+++ src/app/contact/components/contactform.tsx
@@ -4,17 +4,20 @@ import Enterbtn from '../../../../public/images/enterbtn.svg'
 
 const contactform = () => {
   return (
-    <div className='flex justify-between mt-[3em] mb-[7em] '>
+    <div className='flex flex-col lg:flex-row justify-between mt-[3em] mb-[7em] '>
         <div>
             <h1 className='font-[500] text-[40px]'>CONTACT US</h1>
         </div>
         <div className='flex flex-col gap-[3em]'>
-            <h1 className='font-[400] 2xl:text-[25px]'>Have questions, feedback, or just want to say hi? We are here to help!</h1>
+            <h1 className='font-[400] 2xl:text-[25px] text-[25px] lg:text-wrap lg:w-[75%]'>Have questions, feedback, or other complaints? We are here to help!</h1>
             <input type="text" className=' border-b-[1px] w-[80%] border-black py-4 focus:outline-none ' placeholder='ENTER NAME'/>
             <input type="text" className=' border-b-[1px] w-[80%] border-black py-4 focus:outline-none' placeholder='ENTER EMAIL ADDRESS'/>
             <div className='flex  gap-[1em]'>
             <textarea  className=' border-b-[1px] w-[80%] h-[7em] border-black P-5 focus:outline-none' placeholder='HOW CAN  WE HELP?'/>
+            <div className='cursor-pointer hover:rotate-10'>
             <Image src={Enterbtn} alt='enter button'/>
+            </div>
+
 
             </div>
             
