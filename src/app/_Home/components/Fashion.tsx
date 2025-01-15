6e2b49ee9bc89a9b54e2e0b15519f5bdbdae660c
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-01-15 20:28:33 +0100

    fix: improved the responsiveness

---
+++ src/app/_Home/components/Fashion.tsx
@@ -0,0 +1,23 @@
+"use client"
+import React from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const Fashion = () => {
+  const { scrollY } = useScroll();
+  const xPosition = useTransform(scrollY, [0, 1000], ["100%", "-250%"]); // Adjust range for scroll effect
+
+  return (
+    <div className="sm:h-[100vh] md:h-[175vh] lg:h-[160vh]  xl:h-[190vh] ">
+      <div className="sticky top-1/2 -translate-y-1/2 overflow-hidden">
+        <motion.h1
+          className=" md:text-40 lg:text-50 xl:text-62.5 font-bold whitespace-nowrap font-moon"
+          style={{ x: xPosition }}
+        >
+          FASHION WITH PURPOSE
+        </motion.h1>
+      </div>
+    </div>
+  );
+};
+
+export default Fashion;
