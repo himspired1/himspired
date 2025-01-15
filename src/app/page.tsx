1cbc678fcb9a6d1f1abc984c60407626ce93d052
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-01-15 20:28:33 +0100

    fix: improved the responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Fashion from "./_Home/components/Fashion";
 import Newsletter from "./_Home/components/Newsletter";
 import Owners from "./_Home/components/Owners";
 
 export default function Home() {
   return (
     <div>
+      <Fashion />
       <Owners />
       <Newsletter />
     </div>
