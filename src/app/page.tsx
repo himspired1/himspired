165aa384a107de04d493ae43b8754d9c922ef970
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-01-22 14:38:16 +0100

    feat: created the home product section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Fashion from "./_Home/components/Fashion";
 import Newsletter from "./_Home/components/Newsletter";
 import Owners from "./_Home/components/Owners";
+import MainSection from "./_Home/components/ScrollSection";
 
 export default function Home() {
   return (
     <div className="font-activo">
+      <MainSection />
       <Fashion />
       <Owners />
       <Newsletter />
