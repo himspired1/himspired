83dc8013065cd14faf06cac097ef9ea59f8191e6
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-02-26 19:50:24 +0100

    fix: update homepage product

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { GeistSans, GeistMono } from "geist/font";
+import { GeistSans, GeistMono } from 'geist/font'
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Toaster } from "sonner";
-import Loader from "@/components/Loader";
+import Loader from '@/components/Loader'
+
 
 export const metadata: Metadata = {
   title: "Himspired",
@@ -17,12 +18,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en" className={GeistSans.className}>
       <body className={`antialiased min-h-screen flex flex-col`}>
-        <Navbar />
-        <div className="flex-grow">{children}</div>
-        <Footer />
-        <Toaster position="top-right" richColors expand={false} />
+        <Loader />
+        <main 
+          className="opacity-0 animate-fadeIn" 
+          style={{ 
+            animationDelay: '6.5s', 
+            animationFillMode: 'forwards' 
+          }}
+        >
+          <Navbar />
+          <div className="flex-grow">{children}</div>
+          <Footer />
+          <Toaster position="top-right" richColors expand={false} />
+        </main>
       </body>
     </html>
   );
